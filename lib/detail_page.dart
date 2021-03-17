9029b930ab994f2e4f8a02b191397d847c7c3e87
prabhunath111 <prabhutiwari111@gmail.com>
2021-03-17 14:39:38 +0530

    first

---
+++ lib/detail_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:places_app/place.dart';
+import 'package:places_app/places_data.dart';
+
+class DetailPage extends StatelessWidget {
+  final Place place;
+  DetailPage(this.place);
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text(place.subtitle),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Image.asset(
+              place.image,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          titleSection(),
+          buttonSection(color),
+          textSection()
+        ],
+      ),
+    );
+  }
+
+  Widget titleSection() {
+    return Container(
+      padding: EdgeInsets.all(24),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    place.title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                Text(place.subtitle,
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 4.0),
+            child: Text('41'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buttonSection(Color color) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buttonColumn(
+            color, Icons.call, 'CALL'
+          ),
+          buttonColumn(
+              color, Icons.near_me, 'CALL'
+          ),
+          buttonColumn(
+              color, Icons.share, 'CALL'
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget textSection() {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Text(Places().getPlacesDescription()),
+    );
+  }
+
+  buttonColumn(Color color, IconData icon, String label) {
+    return InkWell(
+      onTap: (){},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon, color: color,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 8),
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: color
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/places_data.dart
@@ -0,0 +1,39 @@
+import 'package:places_app/place.dart';
+
+class Places {
+  List<Place> getPlaces() {
+    return [
+      Place(
+        image: 'assets/images/pic0.jpg',
+        title: 'Chennail',
+        subtitle: 'FlowerMarket0',
+      ),
+      Place(
+          image: 'assets/images/pic1.jpg',
+          title: 'Tanjore',
+          subtitle: 'Bronzeworks1'),
+      Place(
+        image: 'assets/images/pic2.jpg',
+        title: 'Chennail1',
+        subtitle: 'FlowerMarket2',
+      ),
+      Place(
+          image: 'assets/images/pic3.jpg',
+          title: 'Tanjore1',
+          subtitle: 'Bronzeworks3'),
+      Place(
+        image: 'assets/images/pic4.jpg',
+        title: 'Chennail2',
+        subtitle: 'FlowerMarket4',
+      ),
+      Place(
+          image: 'assets/images/pic5.jpg',
+          title: 'Tanjore2',
+          subtitle: 'Bronzeworks5'),
+    ];
+  }
+
+  String getPlacesDescription() {
+    return "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.";
+  }
+}
